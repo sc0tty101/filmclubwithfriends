6cbb2c8aba7d10580407cdfab2f262546ffc37b9
Scott Young <young.scott.andrew@gmail.com>
2025-08-02 12:53:02 +1000

    Update setup.js
    Update database path to use Railway volume - /app/filmclub.db for persistence

--- database/setup.js
+++ database/setup.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 
 // Initialize SQLite database
-const db = new sqlite3.Database('./filmclub.db');
+const db = new sqlite3.Database('/app/filmclub.db');
 
 // Create tables when module is loaded
 db.serialize(() => {
